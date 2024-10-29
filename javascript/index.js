24c9c139763c70e20becb357ed4c17cc79d7e657
zoiyaj <z.jesani@gmail.com>
2024-10-29 12:55:14 -0700

    finish JS html injection

--- javascript/index.js
+++ javascript/index.js
@@ -15,5 +15,29 @@ function updateTime() {
   parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
 }
 
+function updateCity(event) {
+  let locationTimeZone = event.target.value;
+  let cityTime = moment().tz(locationTimeZone);
+  let cityName = locationTimeZone.replace("_", " ").split("/")[1];
+  let citiesElement = document.querySelector("#location");
+  citiesElement.innerHTML = `
+</br>
+      <div class="city">
+      <div>
+<h2>${cityName}</h2>
+<div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+      </div>
+      <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}
+        </small></div>
+    </div>
+    </br>
+   `;
+}
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#citySelector");
+
+citiesSelectElement.addEventListener("change", updateCity);
